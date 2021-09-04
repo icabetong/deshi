31f5588b6509015fc4a9c626846003d6ab85db8b
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-09-04 08:40:09 +0800

    Working Status

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+require('dotenv').config();
 const express = require('express');
+const admin = require('firebase-admin');
+
 const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.FIREBASE_TOKEN, 'base64').toString()))
+})
+
+/**
+ *  example type request: 
+ *  const request = {
+ *      notification: { title: "", body: "" },
+ *      deviceToken: "",
+ *      data: {}
+ *  }
+ */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/requests', (request, response) => {
-    console.log('Got body: ', request.body);
+app.post('/notification', (request, response) => {
+    var deviceToken = request.body.deviceToken;
+    if (!deviceToken)
+        return;
+
+    const message = {
+        notification: {
+            title: request.body.notification.title,
+            body: request.body.notification.body
+        },
+        token: deviceToken
+    }
+
+    admin.messaging().send(message)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error))
+
     return response.sendStatus(200);
 })
 
--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -d "username=dax" -X POST http://localhost:5000/requests
-#curl -d '{"name": "dax"}' -H "Content-Type: application/json" http://localhost:5000/requests
+#curl -d "username=dax&password=secret" -X POST http://localhost:5000/requests
+curl -d '{"notification": {"title": "Sample", "body": "Testing"},"deviceToken":"fM0lSaCITpGi4o7VFgmiwK:APA91bFmXaaOOXXdc4dEVEXMHUcMdJVzF6ki1Xcucvhn82wy142JmVQUtAGus3jS-qPkZDB1mKdGTYGYsaaM97bmObzKYzSQT0GGkg4Xp-rLGIytHF_ZMq9S8mSNzvvcKLrBKh9ugi9P"}' -H "Content-Type: application/json" http://localhost:5000/notification
